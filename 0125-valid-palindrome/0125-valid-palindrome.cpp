30c0aedda48e66f2edb32d5451363d4ddd5e46af
Pratiksha Singh <pratikshasingh1537@gmail.com>
2026-08-18 09:38:00 +0530

    Time: 1 ms (43.91%), Space: 10 MB (53.76%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            // Skip non-alphanumeric characters from the left
+            if (!isalnum(s[left])) {
+                left++;
+            }
+            // Skip non-alphanumeric characters from the right
+            else if (!isalnum(s[right])) {
+                right--;
+            }
+            // Both are alphanumeric → compare them
+            else {
+                if (tolower(s[left]) != tolower(s[right])) {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+};
